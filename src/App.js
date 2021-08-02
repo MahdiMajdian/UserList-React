33e51f85e6fd1f913e1595b40fa302ecb154e2fb
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-02 19:55:38 +0430

    AddUser and UsersList components added to the page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react"
+import AddUser from "./components/Users/AddUser"
+import UsersList from "./components/Users/UsersList"
 
 function App() {
-	return <div>hey</div>
+	return (
+		<>
+			<AddUser />
+			<UsersList users={[]} />
+		</>
+	)
 }
 
 export default App
