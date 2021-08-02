36489add4898d2c6c3bdeeea9b72fae710d94a97
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-08-02 19:53:17 +0430

    UsersList component created

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Card from "../UI/Card"
+import styles from "./UsersList.module.css"
+
+const UsersList = (props) => {
+	let listItems = <h2>404 User not found</h2>
+	if (props.users.length) {
+		listItems = props.users.map((item) => (
+			<li>
+				{item.name} ({item.age} years old)
+			</li>
+		))
+	}
+	return (
+		<Card className={styles.users}>
+			<ul>{listItems}</ul>
+		</Card>
+	)
+}
+
+export default UsersList
